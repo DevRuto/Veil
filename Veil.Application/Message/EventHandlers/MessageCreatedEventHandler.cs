41febc4f38a72352d3cb7e75004e7c463147044e
Winston <ruto@protonmail.ch>
2025-02-09 18:42:19 -0500

    Cleanup usings

--- Veil.Application/Message/EventHandlers/MessageCreatedEventHandler.cs
+++ Veil.Application/Message/EventHandlers/MessageCreatedEventHandler.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using MediatR;
+
 using Microsoft.Extensions.Logging;
+
 using Veil.Core.Events;
 
 namespace Veil.Application.Message.EventHandlers;
