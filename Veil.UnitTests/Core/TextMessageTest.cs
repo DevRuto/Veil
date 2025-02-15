e6105a1cae111fe18727a53661d4174b37a77947
Winston <ruto@protonmail.ch>
2025-02-15 17:32:22 -0500

    Update Message UnitTest

--- Veil.UnitTests/Core/TextMessageTest.cs
+++ Veil.UnitTests/Core/TextMessageTest.cs
@@ -1,17 +1,19 @@
 using Shouldly;
 
 using Veil.Core.Entities;
+using Veil.Core.Enums;
 
 namespace Veil.UnitTests.Core;
 
-public class MessageTest
+public class TextMessageTest
 {
     [Fact]
     public void ShouldHaveNewGuid()
     {
         var message = TextMessage.Create("test message");
         
-        message.Value.ShouldBe("test message");
         message.Id.ShouldNotBe(Guid.Empty);
+        message.Value.ShouldBe("test message");
+        message.DataType.ShouldBe(DataType.Text);
     }
 }
