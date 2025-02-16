01202ed83f228cd9d12765e87fe3f76762882df3
Winston (Ruto) <ruto@protonmail.ch>
2025-02-16 11:55:47 -0500

    Recreate webapp using Quasar Cli

--- Veil.WebApp/src/App.vue
+++ Veil.WebApp/src/App.vue
@@ -1,38 +1,7 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-const drawer = ref(true)
-</script>
-
 <template>
-  <q-layout view="hHh Lpr lff">
-    <q-header elavated>
-      <q-toolbar>
-        <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
-        <q-toolbar-title>Header</q-toolbar-title>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer v-model="drawer" show-if-above :width="200" :breakpoint="500" bordered>
-      <q-scroll-area class="fit">
-        <q-list>
-          <q-item clickable :active="true" v-ripple>
-            <q-item-section avatar>
-              <q-icon name="inbox" />
-            </q-item-section>
-            <q-item-section>
-              <RouterLink to="/">Home</RouterLink>
-            </q-item-section>
-          </q-item>
-          <q-separator />
-        </q-list>
-      </q-scroll-area>
-    </q-drawer>
-
-    <q-page-container>
-      <q-page padding>
-        <RouterView />
-      </q-page>
-    </q-page-container>
-  </q-layout>
+  <router-view />
 </template>
+
+<script setup lang="ts">
+//
+</script>
--- Veil.WebApp/src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <main>
-    <h1>Hello, this is home</h1>
-  </main>
-</template>
